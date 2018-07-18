shenki/hostboot

commit 7f96290125b161fae870d02668d90c8c97a48c4d

bootconfig: Describe pnor boot flags
The reserved boot flag is now used by a BMC to signal to the host
firmware that it supports the mbox protocol.

Currently this has no affect on Hostboot, but is used by skiboot.
In the future hostboot could chose to use the mbox protocol to read and
write PNOR.

Change-Id: I51bc7878eabc975954371ada838d0a348b12ef9d
Signed-off-by: Joel Stanley <joel.stanley@au1.ibm.com>
Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/54740
Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

src/usr/initservice/bootconfig/bootconfig_ast2400.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2016                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2018                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -171,6 +171,23 @@ void AST2400BootConfig::configureBootOptions(uint8_t i_bootOptions )
             errlCommit(errl,INITSVC_COMP_ID);
         }
     }
+
+// configurePnorDriver()
+// ----------------------------------------------------------------------------
+void AST2400BootConfig::configurePnorDriver(enum pnorDriverOptions i_pnorDriver )
+{
+    switch (i_pnorDriver) {
+    case MBOX:
+        TRACFCOMP(g_bc_trace,
+                "configurePnorDriver() bmc supports mbox protocol"); }
+	/* fall through - no hostboot support for mbox */
+        TRACFCOMP(g_bc_trace,
+                "configurePnorDriver() hb does not support mbox protocol"); }
+    case SFC:
+        TRACFCOMP(g_bc_trace,
+                "configurePnorDriver() using sfc driver"); }
+	break;
+    }
 }
 
 // ----------------------------------------------------------------------------
@@ -249,6 +266,8 @@ errlHndl_t AST2400BootConfig::readAndProcessBootConfig()
 void AST2400BootConfig::processBootFlagsV1( uint8_t i_flags )
 {
     configureBootMode( i_flags & BOOT_MODE_FLAGS );
+
+    configurePnorDriver( i_flags & PNOR_DRIVER_FLAGS );
 
     configureBootOptions( i_flags & BOOT_OPTIONS_FLAGS );
 

src/usr/initservice/bootconfig/bootconfig_ast2400.H
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2016                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2018                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -75,7 +75,7 @@ class AST2400BootConfig : public BootConfig
     enum bootFlagMask_t
     {
         LOG_LEVEL_FLAGS     = 0x07,
-        RESERVED_FLAGS      = 0x08,
+        PNOR_DRIVER_FLAGS   = 0x08,
         BOOT_OPTIONS_FLAGS  = 0x30,
         BOOT_MODE_FLAGS     = 0xC0,
     };
@@ -86,6 +86,13 @@ class AST2400BootConfig : public BootConfig
         NORMAL             = 0x00,
         TERMINATE_ON_ERROR = 0x40,
         ISTEP_MODE         = 0x80,
+    };
+
+    // Select pnor driver from config register
+    enum pnorDriverOptions
+    {
+        SFC                = 0x00,
+        MBOX               = 0x08,
     };
 
     // Masks to test boot options flags from config register

commit dedad2d8d0f0b671519cc3c9a31a8f3f3053b0f3

bootconfig: Sync with master
Commit 7f96290125b1 ("bootconfig: Describe pnor boot flags") introduced
a build errors.

This fixes those errors, and updates the p8 branch with the same code
that was added to master.

Change-Id: I6625f46db17efe412f6bc973a794b0258fdf76db
Signed-off-by: Joel Stanley <joel.stanley@au1.ibm.com>

src/usr/initservice/bootconfig/bootconfig_ast2400.C
@@ -171,21 +171,30 @@ void AST2400BootConfig::configureBootOptions(uint8_t i_bootOptions )
             errlCommit(errl,INITSVC_COMP_ID);
         }
     }
+}
 
 // configurePnorDriver()
 // ----------------------------------------------------------------------------
-void AST2400BootConfig::configurePnorDriver(enum pnorDriverOptions i_pnorDriver )
+void AST2400BootConfig::configurePnorDriver( uint8_t i_driver )
 {
-    switch (i_pnorDriver) {
+    switch (i_driver) {
     case MBOX:
         TRACFCOMP(g_bc_trace,
-                "configurePnorDriver() bmc supports mbox protocol"); }
+                "configurePnorDriver() bmc supports mbox protocol");
+
+#ifndef CONFIG_PNORDD_IS_BMCMBOX
 	/* fall through - no hostboot support for mbox */
         TRACFCOMP(g_bc_trace,
-                "configurePnorDriver() hb does not support mbox protocol"); }
+                  "configurePnorDriver() hb does not support mbox protocol");
+#else
+	TRACFCOMP(g_bc_trace,
+		  "configurePnorDriver() using mbox driver");
+	break;
+#endif
+
     case SFC:
         TRACFCOMP(g_bc_trace,
-                "configurePnorDriver() using sfc driver"); }
+                "configurePnorDriver() using sfc driver");
 	break;
     }
 }

src/usr/initservice/bootconfig/bootconfig_ast2400.H
@@ -187,6 +187,14 @@ public:
     void configureBootMode( uint8_t i_mode );
 
     /**
+     * @brief Process the pnor driver mode from the boot flags register.
+     *
+     * @param[i] i_driver - pnor driver as set in the boot flags reg.
+     *
+     */
+    void configurePnorDriver( uint8_t i_driver );
+
+    /**
      * @brief Process boot options from boot flags register
      *
      * @param[i] i_options - requested boot options as set in the boot flags